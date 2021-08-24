db3c98e6876e3d85ac8ea6b80bc13db95f8bb6a3
Igor <igorln96@gmail.com>
2021-08-23 22:35:02 -0300

    class 5

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -6,21 +6,46 @@ import {
   Text,
   Image,
   FlatList,
+  Alert,
 } from 'react-native';
 import { Header } from '../components/Header';
 
 import waterdrop from '../assets/waterdrop.png'
 import colors from '../styles/colors';
-import { loadPlant, PlantProps } from '../libs/storage';
+import { loadPlant, PlantProps, removePlant } from '../libs/storage';
 import { formatDistance } from 'date-fns';
 import { pt } from 'date-fns/locale';
 import fonts from '../styles/fonts';
 import { PlantCardSecondary } from '../components/PlantCardSecondary';
+import { Load } from '../components/Load';
 
 export function MyPlants() {
   const [myPlants, setMyPlants]  = useState<PlantProps[]>([]);
   const [loading, setLoading] = useState(true);
   const [nextWater, setNextWater] = useState<string>();
+
+  function handleRemove(plant:PlantProps) {
+    Alert.alert('Remover', `Deseja remover a ${plant.name}?`,[
+      {
+        text: "Não 🙏🏻",
+        style: "cancel",
+      },
+      {
+        text: "Sim 😥",
+        onPress: async () => {
+          try {
+            await removePlant(plant.id);
+           
+            setMyPlants((oldData) =>
+              oldData.filter((item) => item.id !== plant.id)
+            );
+          } catch (error) {
+            Alert.alert("Não foi possível remover! 😥");
+          }
+        },
+      },
+    ]);
+  }
 
   useEffect(() => {
     async function loadStorageData() {
@@ -43,6 +68,9 @@ export function MyPlants() {
 
     loadStorageData()
   },[])
+
+  if(loading)
+    return <Load />
 
   return (
     <View style={styles.container}>
@@ -67,7 +95,10 @@ export function MyPlants() {
           data={myPlants}  
           keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
-            <PlantCardSecondary data={item} />
+            <PlantCardSecondary 
+              data={item}
+              handleRemove={() => {handleRemove(item)}}
+            />
           )}
           showsVerticalScrollIndicator={false}
         />
